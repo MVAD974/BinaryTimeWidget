f46d4ec98e7ee1bd007f281d7e691d4eb5bee665
Marc Vadier <vadiermarc@gmail.com>
2025-11-04 13:46:25 +0400

    Initial Commit

---
+++ BinaryTimeWidget/BinaryTimeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryTimeWidgetApp.swift
+//  BinaryTimeWidget
+//
+//  Created by Marc Vadier on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BinaryTimeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BinaryTimeWidgetTests/BinaryTimeWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryTimeWidgetTests.swift
+//  BinaryTimeWidgetTests
+//
+//  Created by Marc Vadier on 04/11/2025.
+//
+
+import Testing
+@testable import BinaryTimeWidget
+
+struct BinaryTimeWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
